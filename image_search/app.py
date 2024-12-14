24e1478b74220ec9af3d9f3342ded35c76e22c4e
booya <github@incognitus.org>
2024-12-14 08:58:49 -0500

    add newrelic observability

--- image_search/app.py
+++ image_search/app.py
@@ -1,9 +1,13 @@
 from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
+import newrelic.agent
 
 from image_search import api
 from image_search.db import create_db_and_tables, get_or_create_engine
+
+# nrapp = newrelic.agent.application()
+newrelic.agent.initialize()
 
 
 @asynccontextmanager
@@ -18,6 +22,7 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(api.router, prefix="/api")
 
 
+@newrelic.agent.function_trace(name="root")
 @app.get("/")
 async def root():
     return {"message": "Hello World"}
